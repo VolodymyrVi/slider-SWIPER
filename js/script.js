a6a0e867d59a673cad2f130c1d0045834cf76db1
VolodymyrVi <sqlphpjs@gmail.com>
2021-05-19 19:54:12 +0300

    add slider + bullet + pagination

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-const swiper = new Swiper('.image-slider')
+new Swiper('.image-slider', {
+    // стрелки управления
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    // Навигация
+    // Буллеты, текущее положение, прогрессбар
+    pagination: {
+        el: '.swiper-pagination',
+        // Буллеты
+        clickable: true,
+        //Динамические буллеты
+        dynamicBullets: true,
+        //Кастомные буллеты
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+})
