454d335a6f31a1ef5ed2a81d84450cd309af34c0
VolodymyrVi <sqlphpjs@gmail.com>
2021-05-19 21:30:29 +0300

    add swiper pagination frction

--- js/script.js
+++ js/script.js
@@ -8,13 +8,21 @@ new Swiper('.image-slider', {
     // Буллеты, текущее положение, прогрессбар
     pagination: {
         el: '.swiper-pagination',
-        // Буллеты
+        /* // Буллеты
         clickable: true,
         //Динамические буллеты
         dynamicBullets: true,
         //Кастомные буллеты
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
+        }, */
+        // Фракции 
+        type: 'fraction',
+        // Кастомный вывод фракции
+        renderFraction: function (currentClass, totalClass) {
+            return 'Фото <span class ="' + currentClass + '"></span>' + 
+            ' из ' + 
+            '<span class="' + totalClass + '"></span>';
+        }
     },
 })
